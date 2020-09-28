f72359d1cdf0e4937baf3eea57958679743a76bc
Myko <m.miparanum@gmail.com>
2020-09-29 06:24:39 +0800

    added about page component

---
+++ client/src/components/pages/About.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const About = () => {
+  return (
+    <div>
+      <p className='my-1'>A Full stack React app for keeping contact</p>
+      <p className='bg-dark p'>
+        <strong>Version: </strong>1.0.0
+      </p>
+    </div>
+  );
+};
+
+export default About;
