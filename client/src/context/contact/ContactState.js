cfbe15f20bc2e742a3836e80509dcf1b82ef840d
mykomiparanum <m.miparanum@gmail.com>
2020-10-07 22:31:12 +0200

    added add and delete contact

--- client/src/context/contact/ContactState.js
+++ client/src/context/contact/ContactState.js
@@ -1,7 +1,7 @@
-import React, { useReducer } from 'react';
-import uuid from 'uuid';
-import ContactContext from './contactContext';
-import ContactReducer from './contactReducer';
+import React, { useReducer } from "react"
+import { v4 as uuidv4 } from "uuid"
+import ContactContext from "./contactContext"
+import ContactReducer from "./contactReducer"
 import {
   ADD_CONTACT,
   DELETE_CONTACT,
@@ -10,40 +10,48 @@ import {
   UPDATE_CONTACT,
   FILTER_CONTACT,
   CLEAR_FILTER,
-} from '../types';
+} from "../types"
 
 const ContactState = (props) => {
   const initialState = {
     contacts: [
       {
         id: 1,
-        name: 'Myko Miparanum',
-        email: 'mmiparanum@gmail.com',
-        phone: '000-001-1234',
-        type: 'professional',
+        name: "Myko Miparanum",
+        email: "mmiparanum@gmail.com",
+        phone: "000-001-1234",
+        type: "professional",
       },
       {
         id: 2,
-        name: 'Garrette Paran',
-        email: 'gparan@gmail.com',
-        phone: '123-333-1234',
-        type: 'personal',
+        name: "Garrette Paran",
+        email: "gparan@gmail.com",
+        phone: "123-333-1234",
+        type: "personal",
       },
       {
         id: 3,
-        name: 'Mark Duhaylungson',
-        email: 'md@gmail.com',
-        phone: '231-790-7677',
-        type: 'professional',
+        name: "Mark Duhaylungson",
+        email: "md@gmail.com",
+        phone: "231-790-7677",
+        type: "professional",
       },
     ],
-  };
+  }
 
-  const [state, dispatch] = useReducer(ContactReducer, initialState);
+  const [state, dispatch] = useReducer(ContactReducer, initialState)
 
   // Add Contact
+  // create a function that takes contact param
+  const addContact = (contact) => {
+    contact.id = uuidv4()
+    dispatch({ type: ADD_CONTACT, payload: contact })
+  }
 
   // Delete Contact
+  const deleteContact = (id) => {
+    dispatch({ type: DELETE_CONTACT, payload: id })
+  }
 
   // Set Current Contact
 
@@ -59,11 +67,14 @@ const ContactState = (props) => {
     <ContactContext.Provider
       value={{
         contacts: state.contacts,
+
+        addContact,
+        deleteContact,
       }}
     >
       {props.children}
     </ContactContext.Provider>
-  );
-};
+  )
+}
 
-export default ContactState;
+export default ContactState
