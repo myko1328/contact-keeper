72171a03709d2f33b22902e8d0177651e5c393b7
mykomiparanum <m.miparanum@gmail.com>
2020-10-09 01:01:23 +0200

    added login & register navbar

--- client/src/components/layout/Navbar.js
+++ client/src/components/layout/Navbar.js
@@ -1,6 +1,6 @@
-import React from 'react';
-import PropTypes from 'prop-types';
-import { Link } from 'react-router-dom';
+import React from "react"
+import PropTypes from "prop-types"
+import { Link } from "react-router-dom"
 
 const Navbar = ({ title, icon }) => {
   return (
@@ -11,21 +11,29 @@ const Navbar = ({ title, icon }) => {
       <ul>
         <li>
           <Link to='/'>Home</Link>
+        </li>
+        <li>
           <Link to='/about'>About</Link>
+        </li>
+        <li>
+          <Link to='/register'>Register</Link>
+        </li>
+        <li>
+          <Link to='/login'>Login</Link>
         </li>
       </ul>
     </div>
-  );
-};
+  )
+}
 
 Navbar.propTypes = {
   title: PropTypes.string.isRequired,
   icon: PropTypes.string,
-};
+}
 
 Navbar.defaultProps = {
-  title: 'Contact Keeper',
-  icon: 'fas fa-id-card-alt',
-};
+  title: "Contact Keeper",
+  icon: "fas fa-id-card-alt",
+}
 
-export default Navbar;
+export default Navbar
