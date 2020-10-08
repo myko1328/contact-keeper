75e1798cd51ed037178c986b8f46df8cda0dd27a
mykomiparanum <m.miparanum@gmail.com>
2020-10-09 01:01:54 +0200

    initialized authState and add login&reg routes

--- client/src/App.js
+++ client/src/App.js
@@ -1,30 +1,37 @@
-import React, { Fragment } from 'react';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-import Navbar from './components/layout/Navbar';
-import Home from './components/pages/Home';
-import About from './components/pages/About';
+import React, { Fragment } from "react"
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom"
+import Navbar from "./components/layout/Navbar"
+import Home from "./components/pages/Home"
+import About from "./components/pages/About"
+import Register from "./components/auth/Register"
 
-import ContactState from './context/contact/ContactState';
-import './App.css';
+import ContactState from "./context/contact/ContactState"
+import "./App.css"
+import AuthState from "./context/auth/AuthState"
+import Login from "./components/auth/Login"
 
 const App = () => {
   return (
-    <ContactState>
-      <Fragment>
-        <Router>
-          <div className='App'>
-            <Navbar />
-            <div className='container'>
-              <Switch>
-                <Route exact path='/' component={Home} />
-                <Route exact path='/about' component={About} />
-              </Switch>
+    <AuthState>
+      <ContactState>
+        <Fragment>
+          <Router>
+            <div className='App'>
+              <Navbar />
+              <div className='container'>
+                <Switch>
+                  <Route exact path='/' component={Home} />
+                  <Route exact path='/about' component={About} />
+                  <Route exact path='/register' component={Register} />
+                  <Route exact path='/login' component={Login} />
+                </Switch>
+              </div>
             </div>
-          </div>
-        </Router>
-      </Fragment>
-    </ContactState>
-  );
-};
+          </Router>
+        </Fragment>
+      </ContactState>
+    </AuthState>
+  )
+}
 
-export default App;
+export default App
