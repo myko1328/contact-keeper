7ae54b9dcbcb77f60512f28bbc4de4fa730d11b8
mykomiparanum <m.miparanum@gmail.com>
2020-10-09 00:22:02 +0200

    added transistion

--- client/src/components/contacts/Contacts.js
+++ client/src/components/contacts/Contacts.js
@@ -1,19 +1,34 @@
-import React, { Fragment, useContext } from 'react';
-import ContactItem from './ContactItem';
-import ContactContext from '../../context/contact/contactContext';
+import React, { Fragment, useContext } from "react"
+import { CSSTransition, TransitionGroup } from "react-transition-group"
+import ContactItem from "./ContactItem"
+import ContactContext from "../../context/contact/contactContext"
 
 const Contacts = () => {
-  const contactContext = useContext(ContactContext);
+  const contactContext = useContext(ContactContext)
 
-  const { contacts } = contactContext;
+  const { contacts, filtered } = contactContext
+
+  if (contacts.length === 0) {
+    return <h4>Please add contact</h4>
+  }
 
   return (
     <Fragment>
-      {contacts.map((contact) => (
-        <ContactItem contact={contact} key={contact.id} />
-      ))}
+      <TransitionGroup>
+        {filtered !== null
+          ? filtered.map((contact) => (
+              <CSSTransition key={contact.id} timeout={500} classNames='item'>
+                <ContactItem contact={contact} />
+              </CSSTransition>
+            ))
+          : contacts.map((contact) => (
+              <CSSTransition key={contact.id} timeout={500} classNames='item'>
+                <ContactItem contact={contact} />
+              </CSSTransition>
+            ))}
+      </TransitionGroup>
     </Fragment>
-  );
-};
+  )
+}
 
-export default Contacts;
+export default Contacts
