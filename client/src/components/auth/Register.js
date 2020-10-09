6e41ff32d87cf3206c84c2befee01c9b1c88322f
mykomiparanum <m.miparanum@gmail.com>
2020-10-09 23:19:19 +0200

    implement validation with alert

--- client/src/components/auth/Register.js
+++ client/src/components/auth/Register.js
@@ -1,7 +1,11 @@
-import { set } from "mongoose"
-import React, { useState } from "react"
+import React, { useContext, useState } from "react"
+import AlertContext from '../../context/alert/alertContext'
 
 const Register = () => {
+  const alertContext = useContext(AlertContext)
+
+  const { setAlert } = alertContext
+
   const [user, setUser] = useState({
     name: "",
     email: "",
@@ -16,7 +20,13 @@ const Register = () => {
   const onSubmit = (e) => {
     e.preventDefault()
 
-    console.log("Register Submit")
+    if(name === '' || email === '' || password === '') {
+      setAlert('Please enter all fields', 'danger');
+    } else if (password !== password2) {
+      setAlert('Password do not match', 'danger')
+    } else {
+      console.log("Register Submit")
+    }
   }
 
   return (
@@ -27,11 +37,23 @@ const Register = () => {
       <form onSubmit={onSubmit}>
         <div className='form-group'>
           <label htmlFor='name'>Name</label>
-          <input type='text' name='name' value={name} onChange={onChange} />
+          <input 
+            type='text' 
+            name='name' 
+            value={name} 
+            onChange={onChange} 
+            required
+          />
         </div>
         <div className='form-group'>
           <label htmlFor='email'>Email Address</label>
-          <input type='email' name='email' value={email} onChange={onChange} />
+          <input 
+            type='email' 
+            name='email' 
+            value={email} 
+            onChange={onChange} 
+            required
+          />
         </div>
         <div className='form-group'>
           <label htmlFor='password'>Password</label>
@@ -40,6 +62,7 @@ const Register = () => {
             name='password'
             value={password}
             onChange={onChange}
+            minLength='6'
           />
         </div>
         <div className='form-group'>
@@ -49,6 +72,7 @@ const Register = () => {
             name='password2'
             value={password2}
             onChange={onChange}
+            minLength='6'
           />
         </div>
         <input
