d8b8d53a02f836fa3b4c1fbee5e25126f3e18cb2
mykomiparanum <m.miparanum@gmail.com>
2020-10-13 21:25:44 +0200

    fix post endpoint (not registering user)

--- client/src/context/auth/AuthState.js
+++ client/src/context/auth/AuthState.js
@@ -56,7 +56,7 @@ const AuthState = (props) => {
     }
 
     try {
-      const res = await axios.post('/api/auth', formData, config)
+      const res = await axios.post('/api/users', formData, config)
 
       dispatch({
         type: REGISTER_SUCCESS,
